8df78d6e335c6ea5b9aa390961f2c8c8c71826ca
Glauber Brennon <glauberbrennon@gmail.com>
2025-06-28 20:34:27 -0300

    added more test scenarios

--- scraper/tests/scraper/domain/test_court_case.py
+++ scraper/tests/scraper/domain/test_court_case.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 
+import pytest
+
 from scraper.domain.court_case import CourtCaseAmount
 
 
@@ -12,3 +14,66 @@ class TestCourtCaseAmount:
         amount = CourtCaseAmount(gross_principal, interest, lawyer_fees)
 
         assert isinstance(amount, CourtCaseAmount)
+
+    def test_constructor_when_gross_principal_is_none_then_raise_value_error(self):
+        # gross_principal = Decimal(1000)
+        # interest = Decimal(100)
+        # lawyer_fees = Decimal(50)
+        gross_principal = None
+        interest = Decimal(100)
+        lawyer_fees = Decimal(50)
+
+        with pytest.raises(ValueError) as _:
+            CourtCaseAmount(gross_principal, interest, lawyer_fees)
+
+    def test_constructor_when_interest_is_none_then_raise_value_error(self):
+        # gross_principal = Decimal(1000)
+        # interest = Decimal(100)
+        # lawyer_fees = Decimal(50)
+        gross_principal = Decimal(1000)
+        interest = None
+        lawyer_fees = Decimal(50)
+
+        with pytest.raises(ValueError) as _:
+            CourtCaseAmount(gross_principal, interest, lawyer_fees)
+
+    def test_constructor_when_lawyer_fees_is_none_then_raise_value_error(self):
+        # gross_principal = Decimal(1000)
+        # interest = Decimal(100)
+        # lawyer_fees = Decimal(50)
+        gross_principal = Decimal(1000)
+        interest = Decimal(100)
+        lawyer_fees = None
+
+        with pytest.raises(ValueError) as _:
+            CourtCaseAmount(gross_principal, interest, lawyer_fees)
+
+    def test_total_when_all_fields_are_set_then_return_correct_total(self):
+        gross_principal = Decimal(1000)
+        interest = Decimal(100)
+        lawyer_fees = Decimal(50)
+
+        amount = CourtCaseAmount(gross_principal, interest, lawyer_fees)
+
+        expected_total = Decimal(1000 + 100 + 50)
+        assert amount.total == expected_total
+
+    def test_total_when_any_field_is_zero_then_return_correct_total(self):
+        gross_principal = Decimal(0)
+        interest = Decimal(100)
+        lawyer_fees = Decimal(50)
+
+        amount = CourtCaseAmount(gross_principal, interest, lawyer_fees)
+
+        expected_total = Decimal(0 + 100 + 50)
+        assert amount.total == expected_total
+
+    def test_total_when_all_fields_are_zero_then_return_zero(self):
+        gross_principal = Decimal(0)
+        interest = Decimal(0)
+        lawyer_fees = Decimal(0)
+
+        amount = CourtCaseAmount(gross_principal, interest, lawyer_fees)
+
+        expected_total = Decimal(0 + 0 + 0)
+        assert amount.total == expected_total
