ebda432fdc50dc0cff2787eabe07376f6d7b7bfe
Glauber Brennon <glauberbrennon@gmail.com>
2025-06-28 21:43:21 -0300

    imppl ExtractAndPersistCourtDataService
    - i decided to violate SRP on this impl because of the deadline.
    - multiple scenarios were tested

---
+++ scraper/tests/scraper/application/test_extract_and_persist_court_data_service.py
@@ -0,0 +1,93 @@
+from unittest import mock
+
+import pytest
+
+from scraper.application.services.extract_and_persist_court_data_service import (
+    ExtractAndPersistCourtDataService,
+)
+from scraper.domain.court_case import CourtCase
+from scraper.domain.ports.court_case_extractor import CourtCaseExtractor
+from scraper.domain.ports.court_case_repository import CourtCaseRepository
+
+
+class TestExtractAndPersistCourtDataService:
+    def test_execute_when_extract_returns_empty_list_then_save_is_not_called(
+        self,
+    ):
+        # Arrange
+        mock_court_case_extractor = mock.Mock(spec=CourtCaseExtractor)
+        mock_court_case_repository = mock.Mock(spec=CourtCaseRepository)
+        mock_court_case_extractor.extract.return_value = []
+        service = ExtractAndPersistCourtDataService(
+            court_case_extractor=mock_court_case_extractor,
+            court_case_repository=mock_court_case_repository,
+        )
+
+        # Act
+        service.execute()
+
+        # Assert
+        mock_court_case_repository.save.assert_not_called()
+
+    def test_execute_when_extract_returns_cases_then_calls_save_for_each_case(
+        self,
+    ):
+        # Arrange
+        mock_court_case_extractor = mock.Mock(spec=CourtCaseExtractor)
+        mock_court_case_repository = mock.Mock(spec=CourtCaseRepository)
+        mock_cases = [
+            mock.Mock(spec=CourtCase),
+            mock.Mock(spec=CourtCase),
+        ]
+        mock_court_case_extractor.extract.return_value = mock_cases
+        service = ExtractAndPersistCourtDataService(
+            court_case_extractor=mock_court_case_extractor,
+            court_case_repository=mock_court_case_repository,
+        )
+
+        # Act
+        service.execute()
+
+        # Assert
+        expected_call_args = [
+            mock.call(mock_cases[0]),
+            mock.call(mock_cases[1]),
+        ]
+        mock_court_case_repository.save.assert_has_calls(expected_call_args)
+
+    def test_execute_when_extractor_raises_exception_then_let_it_raise(
+        self,
+    ):
+        # Arrange
+        mock_court_case_extractor = mock.Mock(spec=CourtCaseExtractor)
+        mock_court_case_repository = mock.Mock(spec=CourtCaseRepository)
+        mock_court_case_extractor.extract.side_effect = Exception("Extraction failed")
+        service = ExtractAndPersistCourtDataService(
+            court_case_extractor=mock_court_case_extractor,
+            court_case_repository=mock_court_case_repository,
+        )
+
+        # Act & Assert
+        with pytest.raises(Exception) as _:
+            service.execute()
+
+    def test_execute_when_repository_raises_exception_then_let_it_raise(
+        self,
+    ):
+        # Arrange
+        mock_court_case_extractor = mock.Mock(spec=CourtCaseExtractor)
+        mock_court_case_repository = mock.Mock(spec=CourtCaseRepository)
+        mock_cases = [
+            mock.Mock(spec=CourtCase),
+            mock.Mock(spec=CourtCase),
+        ]
+        mock_court_case_extractor.extract.return_value = mock_cases
+        mock_court_case_repository.save.side_effect = Exception("Save failed")
+        service = ExtractAndPersistCourtDataService(
+            court_case_extractor=mock_court_case_extractor,
+            court_case_repository=mock_court_case_repository,
+        )
+
+        # Act & Assert
+        with pytest.raises(Exception) as _:
+            service.execute()
