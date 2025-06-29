8ec0c2de9dca9067a103cbae5b183afa0ea8b9a0
Glauber Brennon <glauberbrennon@gmail.com>
2025-06-29 15:11:19 -0300

    refactor on CourtCaseExtractor port

--- scraper/src/scraper/domain/ports/court_case_extractor.py
+++ scraper/src/scraper/domain/ports/court_case_extractor.py
@@ -1,8 +1,17 @@
 """Abstract base class for court scrapers."""
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 from scraper.domain.court_case import CourtCase
+
+
+@dataclass(frozen=True)
+class CourtCaseExtractorFilters:
+    from_date: str
+    to_date: str
+    note_id: int
+    additional_terms: str
 
 
 class CourtCaseExtractor(ABC):
@@ -14,7 +23,7 @@ class CourtCaseExtractor(ABC):
     """
 
     @abstractmethod
-    def extract(self) -> list[CourtCase]:
+    def extract(self, filters: CourtCaseExtractorFilters) -> list[CourtCase]:
         """
         Extract case from the court.
         This method should be implemented by subclasses to define the extraction logic.
